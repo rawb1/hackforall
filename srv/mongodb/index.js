4a00ea59e4286c551af142fb3043adc0745814e0
Robin Perso <robinperso@MBP-de-Robin.localdomain>
2020-01-06 21:23:20 +0100

    fix mongoose erro handling

--- srv/mongodb/index.js
+++ srv/mongodb/index.js
@@ -17,13 +17,18 @@ const database = () => {
   mongoose.set('useUnifiedTopology', true);
   mongoose.set('useNewUrlParser', true);
   mongoose.set('useCreateIndex', true);
-  mongoose.connect(config.mongo.uri);
-
+  mongoose.connect(config.mongo.uri).catch(err => {
+    logger.fatal(`${err.name} :  ${err.message}`);
+    process.exit(1);
+  });
   mongoose.connection.on('disconnected', () => {
-    mongoose.connect(config.mongo.uri);
+    logger.warn('Disconnected fom MongoDB ', config.mongo.uri);
+  });
+  mongoose.connection.on('reconnected', () => {
+    logger.info('Reconnected to MongoDB ', config.mongo.uri);
   });
   mongoose.connection.on('error', err => {
-    logger.fatal(err);
+    logger.err(`${err.name} :  ${err.message}`);
   });
   mongoose.connection.on('open', () => {
     logger.info('Connected to MongoDB ', config.mongo.uri);
