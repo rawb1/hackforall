7c859acae1edf507029de2c610f171bba4394ef7
rawb1 <proquotrobin@gmail.com>
2020-12-02 18:16:12 +0100

    timeline implemented

--- src/components/TimelineCard.vue
+++ src/components/TimelineCard.vue
@@ -1,42 +1,144 @@
 <template>
   <div class="card">
     <div class="card-content">
-      <div class="timeline">
+      <div v-if="!$apollo.queries.hackathon.loading" class="timeline">
         <header class="timeline-header">
-          <span class="tag is-medium is-primary">Start</span>
+          <span class="tag is-primary">
+            {{ new Date(hackathon.dates.start).getFullYear() }}
+          </span>
         </header>
+        <TimelineNow
+          v-if="now < new Date(hackathon.dates.applications.open)"
+          :date="now"
+          :options="options"
+        />
         <div class="timeline-item">
-          <div class="timeline-marker"></div>
-          <div class="timeline-content">
-            <p class="heading">January 2016</p>
-            <p>Timeline content - Can include any HTML element</p>
-          </div>
-        </div>
-        <div class="timeline-item">
-          <div class="timeline-marker is-image is-32x32">
-            <img src="https://bulma.io/images/placeholders/32x32.png" />
+          <div class="timeline-marker is-success is-icon">
+            <i class="far fa-address-card"></i>
           </div>
           <div class="timeline-content">
-            <p class="heading">February 2016</p>
-            <p>Timeline content - Can include any HTML element</p>
+            <p class="heading">
+              {{ open }}
+            </p>
+            <p>Applications open</p>
           </div>
         </div>
-        <header class="timeline-header">
-          <span class="tag is-primary">2017</span>
-        </header>
+        <TimelineNow
+          v-if="
+            new Date(hackathon.dates.applications.open) < now &&
+              now < new Date(hackathon.dates.applications.close)
+          "
+          :date="now"
+          :options="options"
+        />
         <div class="timeline-item">
-          <div class="timeline-marker is-icon">
-            <i class="fa fa-flag"></i>
+          <div class="timeline-marker is-warning is-icon">
+            <i class="far fa-address-card"></i>
           </div>
           <div class="timeline-content">
-            <p class="heading">March 2017</p>
-            <p>Timeline content - Can include any HTML element</p>
+            <p class="heading">
+              {{ close }}
+            </p>
+            <p>Applications close</p>
           </div>
         </div>
+
+        <TimelineNow
+          v-if="
+            new Date(hackathon.dates.applications.close) < now &&
+              now < new Date(hackathon.dates.start)
+          "
+          :date="now"
+          :options="options"
+        />
+        <div class="timeline-item">
+          <div class="timeline-marker is-primary is-icon">
+            <i class="fas fa-code"></i>
+          </div>
+          <div class="timeline-content">
+            <p class="heading">
+              {{ start }}
+            </p>
+            <p>Hackathon start</p>
+          </div>
+        </div>
+        <TimelineNow
+          v-if="
+            new Date(hackathon.dates.start) < now &&
+              now < new Date(hackathon.dates.end)
+          "
+          :date="now"
+          :options="options"
+        />
+        <div class="timeline-item">
+          <div class="timeline-marker is-danger is-icon">
+            <i class="fas fa-trophy"></i>
+          </div>
+          <div class="timeline-content">
+            <p class="heading">
+              {{ end }}
+            </p>
+            <p>Hackathon end</p>
+          </div>
+        </div>
+        <TimelineNow
+          v-if="new Date(hackathon.dates.end) < now"
+          :date="now"
+          :options="options"
+        />
         <div class="timeline-header">
-          <span class="tag is-medium is-primary">End</span>
+          <span class="tag is-primary">End</span>
         </div>
       </div>
     </div>
   </div>
 </template>
+
+<script>
+import TimelineNow from '@/components/TimelineNow.vue';
+import { HACKATHON_QUERY } from '@/graphql/hackathonQueries';
+const INTERVAL = 60 * 1000;
+export default {
+  components: { TimelineNow },
+  data: () => ({
+    now: new Date(),
+    options: {
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric'
+    }
+  }),
+  computed: {
+    open() {
+      return new Date(
+        this.hackathon.dates.applications.open
+      ).toLocaleDateString(undefined, this.options);
+    },
+    close() {
+      return new Date(
+        this.hackathon.dates.applications.close
+      ).toLocaleDateString(undefined, this.options);
+    },
+    start() {
+      return new Date(this.hackathon.dates.start).toLocaleDateString(
+        undefined,
+        this.options
+      );
+    },
+    end() {
+      return new Date(this.hackathon.dates.end).toLocaleDateString(
+        undefined,
+        this.options
+      );
+    }
+  },
+  mounted() {
+    setInterval(() => {
+      this.now = new Date();
+    }, INTERVAL);
+  },
+  apollo: {
+    hackathon: HACKATHON_QUERY
+  }
+};
+</script>
---
+++ src/components/TimelineNow.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="timeline-item">
+    <div class="timeline-marker"></div>
+    <div class="timeline-content">
+      <p class="heading">
+        Today {{ date.toLocaleDateString(undefined, options) }}
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    date: { type: Date, default: new Date() },
+    options: { type: Object, default: () => {} }
+  }
+};
+</script>
