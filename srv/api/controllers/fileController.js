5884bea29355b9a3c03267f4bd72c3d2a95a23d4
rawb1 <proquotrobin@gmail.com>
2020-12-02 18:16:24 +0100

    fix file handling

--- srv/api/controllers/fileController.js
+++ srv/api/controllers/fileController.js
@@ -1,26 +1,31 @@
+const { ApolloError } = require('apollo-server-koa');
+
 const minioClient = require('../../config/minio');
 const { minio } = require('../../config/env');
 
-const _fileName = user => `${user.username}-${user.email}.pdf`;
-
 const write = async (upload, bucket, user) => {
   const file = await upload;
-
   const stream = file.createReadStream();
 
-  await minioClient.putObject(bucket, _fileName(user), stream);
+  await minioClient.putObject(
+    bucket,
+    `${user.id}_${user.username}-${user.email}.pdf`,
+    stream
+  );
 
   return { name: file.filename, type: file.mimetype, bucket };
 };
 
 const read = async (bucket, user) => {
-  const name = `${bucket}-${_fileName(user)}`;
-  const link = await minioClient.presignedGetObject(
-    bucket,
-    _fileName(user),
-    10
-  );
-  return { name, link };
+  const stream = minioClient.listObjectsV2(bucket, user.id);
+  stream.on('error', err => {
+    throw new ApolloError(err);
+  });
+  stream.on('data', async doc => {
+    const name = `${bucket}-${user.username}-${user.email}`;
+    const link = await minioClient.presignedGetObject(bucket, doc.name, 10);
+    return { name, link };
+  });
 };
 
 const serverLink = () =>
